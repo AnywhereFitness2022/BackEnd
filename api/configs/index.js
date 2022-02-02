c4d0440670acd00c985860ebe7f085177734a2c1
Lia Moua <moua0061@yahoo.com>
2022-02-02 14:04:01 -0500

    turns out i need the congif file for other files

---
+++ api/configs/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PORT:'9000',
+    NODE_ENV: 'development',
+    DEV_DATABASE_URL: 'postgresql://postgres:Mm00260618@localhost:5432/clients',
+    TESTING_DATABASE_URL: 'postgresql://postgres:Mm00260618@localhost:5432/clients-test',
+    SEED_PASSWORD: '1234',
+    BCRYPT_ROUNDS: '8',
+    JWT_SECRET: 'this is a secret'
+}
